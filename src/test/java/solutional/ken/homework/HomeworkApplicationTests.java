20fd9778127affd51e42f59d8cca6e0a3a7e2fc3
Ken Rebane <rebaneken95@gmail.com>
2023-11-15 10:04:34 +0200

    initial

---
+++ src/test/java/solutional/ken/homework/HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package solutional.ken.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
